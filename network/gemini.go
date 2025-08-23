6edf087d1adecf20a6b7d906aa561ef74d34b691
0tak2 <0tak2.code@gmail.com>
2025-08-23 23:36:03 +0900

    Feat: 프롬프팅 개선

--- network/gemini.go
+++ network/gemini.go
@@ -87,7 +87,30 @@ func (r geminiRequest) requestToGemini(prompt string) (*GeminiMenuRecommendation
 	return &response, nil
 }
 
+func preprocessLLMInputs(values GeminiMenuRecommendationPromptValues) GeminiMenuRecommendationPromptValues {
+	switch values.Category {
+	case "아시안":
+		values.Category = "인도 음식, 태국 음식, 베트남 음식, 네팔 음식, 몽골 음식"
+	case "세계음식":
+		values.Category = "터키 등 중동 음식, 멕세코 등 남미 음식, 샤실리크 등 러시아 음식, 그 외 아시안 음식을 제외한 세계 음식"
+	case "볶음":
+		values.Category = "볶음 요리"
+	case "밥":
+		values.Category = "덮밥, 비빔밥, 볶음밥 등 각종 밥 요리"
+	case "면":
+		values.Category = "면 요리"
+	case "야채":
+		values.Category = "샐러드, 포케 등 야채가 풍부하게 들어간 음식"
+	case "빠른 식사":
+		values.Category = "분식, 햄버거, 샌드위치 등 빠르게 주문해서 먹을 수 있는 음식. 컵밥은 제외."
+	}
+
+	return values
+}
+
 func CreatePrompt(values GeminiMenuRecommendationPromptValues) (*string, error) {
+	processedValues := preprocessLLMInputs(values)
+
 	tmpl := `Recommend one new menu item that falls under the following category and is not included in the exclusion menu, along with the reason. Please respond in Korean.
 
 Category: {{ .Category }}
@@ -97,7 +120,7 @@ Exclusion Menu: {{range .SkipMenus}}{{.}}, {{end}}
 	t := template.Must(template.New("MenuRecommendRequestPrompt").Parse(tmpl))
 
 	var buf bytes.Buffer
-	if err := t.Execute(&buf, values); err != nil {
+	if err := t.Execute(&buf, processedValues); err != nil {
 		return nil, err
 	}
 
