01837befdbcfe4ea2c2bf7b90db5ab29a568217b
0tak2 <0tak2.code@gmail.com>
2026-02-07 16:27:46 +0900

    Chore: Redis 연결을 위한 설정 추가

--- configuration/config.go
+++ configuration/config.go
@@ -6,28 +6,37 @@ import (
 )
 
 const (
-	portKey        = "AFTERHEE_PORT"
-	dbPathKey      = "AFTERHEE_DUCKDB_FILENAME"
-	neisAPIKey     = "AFTERHEE_NEIS_API_KEY"
-	geminiAPIKey   = "AFTERHEE_GEMINI_API_KEY"
-	runningProfile = "AFTERHEE_PROFILE"
+	portKey          = "AFTERHEE_PORT"
+	dbPathKey        = "AFTERHEE_DUCKDB_FILENAME"
+	redisHostKey     = "AFTERHEE_REDIS_HOST"
+	redisPortKey     = "AFTERHEE_REDIS_PORT"
+	redisPasswordKey = "AFTERHEE_REDIS_PASSWORD"
+	neisAPIKey       = "AFTERHEE_NEIS_API_KEY"
+	geminiAPIKey     = "AFTERHEE_GEMINI_API_KEY"
+	runningProfile   = "AFTERHEE_PROFILE"
 )
 
 type Configuration struct {
-	Port         string
-	DBPath       string
-	NEISAPIKey   string
-	GeminiAPIKey string
-	IsDevMode    bool
+	Port          string
+	DBPath        string
+	RedisHost     string
+	RedisPort     string
+	RedisPassword string
+	NEISAPIKey    string
+	GeminiAPIKey  string
+	IsDevMode     bool
 }
 
 func GetConfiguration() Configuration {
 	return Configuration{
-		Port:         getEnv(portKey, "8080"),
-		DBPath:       getEnv(dbPathKey, "database/db.duckdb"),
-		NEISAPIKey:   getEnv(neisAPIKey, ""),
-		GeminiAPIKey: getEnv(geminiAPIKey, ""),
-		IsDevMode:    getEnv(runningProfile, "PRODUCTION") == "DEV",
+		Port:          getEnv(portKey, "8080"),
+		DBPath:        getEnv(dbPathKey, "database/db.duckdb"),
+		RedisHost:     getEnv(redisHostKey, "localhost"),
+		RedisPort:     getEnv(redisPortKey, "56379"),
+		RedisPassword: getEnv(redisPasswordKey, ""),
+		NEISAPIKey:    getEnv(neisAPIKey, ""),
+		GeminiAPIKey:  getEnv(geminiAPIKey, ""),
+		IsDevMode:     getEnv(runningProfile, "PRODUCTION") == "DEV",
 	}
 }
 
