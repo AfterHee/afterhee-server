5fa4d051ac803ee05d2f83b2d5e8b66612ec10fa
0tak2 <0tak2.code@gmail.com>
2026-02-07 16:28:07 +0900

    Feat: Redis에 읽기 쓰기를 수행하는 CacheRepository 구현

---
+++ repository/cache_repository.go
@@ -0,0 +1,43 @@
+package repository
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+)
+
+type CacheRepository interface {
+	GetValue(ctx context.Context, key string) (*string, error)
+	SetValue(ctx context.Context, key string, value string, expiration time.Duration) error
+}
+
+type cacheRepository struct {
+	rdb *redis.Client
+}
+
+func NewCacheRepository(rdb *redis.Client) CacheRepository {
+	return &cacheRepository{rdb: rdb}
+}
+
+func (s *cacheRepository) GetValue(ctx context.Context, key string) (*string, error) {
+	val, err := s.rdb.Get(ctx, key).Result()
+	if err == redis.Nil {
+		return nil, nil
+	} else if err != nil {
+		log.Printf("falied to fetch data from redis. key=%s error=%s\n", key, err.Error())
+		return nil, err
+	}
+
+	return &val, nil
+}
+
+func (s *cacheRepository) SetValue(ctx context.Context, key string, value string, expiration time.Duration) error {
+	err := s.rdb.SetNX(ctx, key, value, expiration).Err()
+	if err != nil {
+		log.Printf("falied to put data to redis. key=%s value=%s error=%s\n", key, value, err.Error())
+		return err
+	}
+	return nil
+}
